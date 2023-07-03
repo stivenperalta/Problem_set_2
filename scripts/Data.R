bc2891cd44d58988254b537a13d7d1c8fd2badfa
Jazmine Galdos <jazminegaldos@gmail.com>
2023-07-03 18:39:33 -0500

    Update Data.R

--- scripts/Data.R
+++ scripts/Data.R
@@ -18,9 +18,30 @@ path_folder<-dirname(path_script)
 setwd(path_folder)
 getwd()
 
-
-
 # Importing Data ----------------------------------------------------------
 
 #training data
-training<-read_csv(../stores/)
+training<-read_csv("../stores/train.csv")
+names(training) #vemos las variables disponibles
+summary(training)
+
+##Revisamos variables en la base inicial
+
+#CITY
+table(training$city) #revisamos que no hayan errores de entrada en esta variable, todas son Bogotá D.C
+
+#PRICE
+br = seq(3.000e+08,1.650e+09,by=50000000) # creamos los braquets/bins
+ranges = paste(head(br,-1), br[-1], sep=" - ") #creamos los rangos en base a los braquets
+freq   = hist(training$price, breaks=br, include.lowest=TRUE, plot=TRUE, 
+              col="lightblue", border="grey",
+              xlab="Precio en COP", ylab="Número de inmuebles",main="Distribución de Precios en Bogotá") #sacamos la frecuencia para cada uno de los braquets
+
+data.frame(range = ranges, frequency = freq$counts) #tabla de frecuencia con bins
+
+#MONTH YEAR- CREAMOS UNA VARIABLE UNIENDO MES Y AÑO
+typeof(c(training$month, training$year))#revisamos que tipo son (double)
+
+
+
+
