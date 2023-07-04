9eb972e739b5f6d8be00d5d1007c5fc00e241874
Jazmine Galdos <jazminegaldos@gmail.com>
2023-07-04 16:57:19 -0500

    Update Data.R

--- scripts/Data.R
+++ scripts/Data.R
@@ -9,7 +9,7 @@ rm(list = ls()) # Limpiar Rstudio
 require(pacman)
 p_load(ggplot2, rio, tidyverse, skimr, caret, 
        rvest, magrittr, rstudioapi, stargazer, 
-       boot, readxl, knitr, kableExtra, glmnet) # Cargar varios paquetes al tiempo
+       boot, readxl, knitr, kableExtra, glmnet, sf, tmaptools, leaflet) # Cargar varios paquetes al tiempo
 
 
 #Definir el directorio
@@ -19,6 +19,10 @@ setwd(path_folder)
 getwd()
 
 # Importing Data ----------------------------------------------------------
+
+
+
+
 
 #train data
 train<-read_csv("../stores/train.csv")
@@ -32,7 +36,30 @@ db<-rbind(test,train) #juntamos ambas bases
 names(db) #vemos las variables disponibles
 summary(db)
 
-##Revisamos variables en la base inicial
+
+# Setting the location ---------------------------------------------------
+db<- st_as_sf( #para convertirlo en un spatial data frame
+  db,
+  coords = c("lon", "lat"), #primero longitud, luego latitud
+  crs = 4326 #EPSG:4326=WGS84 (World Geodetic System 1984)
+)
+
+
+pal <- colorFactor(
+  palette = c('#d9bf0d', '#00b6f1'),
+  domain = db$sample #variable for which the color vector should be applied to
+)
+
+map<-leaflet() %>% 
+  addTiles() %>%  #capa base
+  addCircles(data=db,col=~pal(sample))%>% #pintar casas en base ala funcion pal que creamos arriba
+  
+map 
+
+
+
+# Checking existing variables ---------------------------------------------
+
 
 #CITY
 table(db$city) #revisamos que no hayan errores de entrada en esta variable, todas son Bogotá D.C
@@ -42,7 +69,7 @@ br = seq(3.000e+08,1.650e+09,by=50000000) # creamos los braquets/bins
 ranges = paste(head(br,-1), br[-1], sep=" - ") #creamos los rangos en base a los braquets
 freq   = hist(train$price, breaks=br, include.lowest=TRUE, plot=TRUE, 
               
-              col="lightblue", border="grey",
+              col="#00b6f1", border="#d9bf0d",
               xlab="Precio en COP", ylab="Número de inmuebles",main="Distribución de Precios en Bogotá") #sacamos la frecuencia para cada uno de los braquets
 
 data.frame(range = ranges, frequency = freq$counts) #tabla de frecuencia con bins
@@ -50,7 +77,7 @@ data.frame(range = ranges, frequency = freq$counts) #tabla de frecuencia con bin
 #Scatterplot de precios por tipo de vivienda (apartamento/casa) (para train)
 ggplot(train, aes(x = surface_total, y = price, color = property_type)) +
   geom_point(size = 2) +
-  scale_color_manual(values = c("lightblue","grey")) +
+  scale_color_manual(values = c("#00b6f1","#d9bf0d")) +
   labs(x = "Superficie Total", y = "Precio", title = "Precios de Inmuebles por superficie")
 
 
@@ -63,21 +90,21 @@ train$date<-as.Date(paste(db$year, db$month,"1", sep = "-")) #se creo variable c
 #SURFACE COVERED
 
 #ROOMS
-table(db$rooms)
+table(train$rooms)
 
 #BEDROOMS
-table(db$bedrooms)
+table(train$bedrooms)
 
 #BATHROOMS
-table(db$bathrooms)
+table(train$bathrooms)
 
 #PROPERTY TYPE
-pt<-data.frame(table(db$property_type))
+pt<-data.frame(table(train$property_type))
 pie_pt <- ggplot(pt, aes(x = "", y = Freq, fill = Var1)) +
   geom_bar(stat = "identity", width = 1) +
   coord_polar("y", start = 0) +
   theme_void() +
-  scale_fill_manual(values = c("#F8766D", "#00BFC4")) +
+  scale_fill_manual(values = c("#d9bf0d", "#00b6f1")) +
   labs(title = "Pie Chart")
 
 pie_pt
