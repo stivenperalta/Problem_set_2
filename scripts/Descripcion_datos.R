deff6e7a1f8b71fe6e219620c930c09149daf916
Jazmine Galdos <jazminegaldos@gmail.com>
2023-07-14 18:51:46 -0500

    Update Descripcion_datos.R

--- scripts/Descripcion_datos.R
+++ scripts/Descripcion_datos.R
@@ -41,22 +41,54 @@ db%>% tabyl(price) #simple table with n, percentage and valid percent
 db%>%tabyl(LOCALIDAD,sample) #cross-tabulation
 db%>%tabyl(LOCALIDAD,sample, property_type) #cross-tabulation
 
-db<-db %>% select (sample, price, bedrooms, property_type, estudio, parqueadero, balcon, chimenea, 
-                   ascensor, bbq, gimnasio, vigilancia, jardin, parrilla, cuarto.servicio, 
-                   conjunto.cerrado, zona.servicio, area, banos,LOCALIDAD, i_riñas, i_orden, 
+db<-db %>% select (sample, price, bedrooms, property_type, parqueadero, balcon, chimenea, 
+                   ascensor, bbq, gimnasio, vigilancia, 
+                   area, banos,LOCALIDAD, i_riñas, i_orden, 
                    i_narcoticos, i_maltrato, d_hurto_personas, d_hurto_autos, d_hurto_motos,
-                   d_violencia, dist_CC, dist_col)
+                   d_violencia, dist_CC, dist_col) #seleccionamos variables que queremos usar para tabla descriptiva
 
-db %>% 
-  tbl_summary(
-    by=sample, #estratificación de tabla
-    statistic= list(all_continuous()~ "{mean}({sd})", #stats y formato para variables continuas
-                    all_categorical()~ "{n}/{N} ({p}%)", #stats y formato para variables categóricas
-                    digits=all_continuous() ~ 1, #redondeo para variables continuas
-                    type=all_categorical()~ "categorical" # forzamos a que se muestren todas las variables categóricas
-                    #label= list( #mostrar lables para las columnas
-                      
-                    )
-)
+db <- db %>%
+  mutate(across(where(is.numeric), ~ round(., digits = 1))) #redondeamos valores
+
+db<- db %>%
+  mutate(price= price/1000000) #ponemos los precios en millones
+
+tbl_summary(db, 
+            by = sample, #sacamos valores separados para train y test
+            statistic = list(all_continuous() ~ "{mean} ({sd})", #estadisticas para valores continuos
+                             all_categorical() ~ "{n}/{N} ({p}%)"),  #estadisticas para variables categoricas
+            digits = all_continuous() ~ 1, 
+            type = all_categorical() ~ "categorical",
+            missing_text= "Missing",
+            label= list(
+              price ~ "Precio en millones COP",
+              bedrooms ~ "Habitaciones",
+              property_type ~ "Tipo de propiedad",
+              parqueadero ~ "Parqueadero",
+              balcon ~ "Balcon",
+              chimenea ~ "Chimenea",
+              ascensor ~ "Ascensor",
+              bbq ~ "BBQ",
+              gimnasio ~ "Gimnasio",
+              vigilancia ~ "Vigilancia",
+              area ~ "Área",
+              banos ~ "Baños",
+              LOCALIDAD ~ "Localidad",
+              i_riñas ~ "Incidentes riñas 2019-2021",
+              i_orden ~ "Incidentes orden público 2019-2021",
+              i_narcoticos~ "Incidentes narcóticos 2019-2021",
+              i_maltrato ~ "Incidentes maltrato 2019-2021",
+              d_hurto_personas ~ "Delitos hurto personas 2019-2021",
+              d_hurto_autos ~ "Delitos hurto autos 2019-2021",
+              d_hurto_motos ~ "Delitos hurto motos 2019-2021",
+              d_violencia ~ "Delitos violencia intrafamiliar 2019-2021",
+              dist_CC ~ "Distancia centro comercial",
+              dist_col ~ "Distancia colegios")
+              
+              
+              
+              
+            )
+
 
 #price, bedrooms, property_type, sample,estudio, parqueadero, balcon, chimenea, ascensor, bbq, gimnasio, vigilancia, jardin, parrilla, cuarto.servicio, conjunto.cerrado, zona.servicio, area, banos,localidad, i.riñas, i.orden, i.narcoticos, i.maltrato, d_hurto_personas, d_hurto_autos, d_hurto_motos,d_violencia, dist_CC, dist_col,
