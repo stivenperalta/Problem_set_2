1b484a822e0a2a0b4eebd73e7778bf90fff78218
ayclavijo <ay.clavijo@uniandes.edu.co>
2023-07-14 22:26:22 -0500

    Update Lasso&Ridge.R

--- scripts/Lasso&Ridge.R
+++ scripts/Lasso&Ridge.R
@@ -11,25 +11,17 @@ rm(list = ls())
 install.packages("pacman")
 install.packages("mice")
 library("mice")
-require(stargazer)
 library("pacman")
-p_load("tidyverse", "sf", "naniar", "tidymodels", "readxl", "psych","ranger","glmnet","naniar","tidyverse", "caret", "glmnet", "ggplot2","ggraph","gt","vtable","spatialsample")
+p_load("tidyverse", "sf", "naniar", "tidymodels", "readxl", "psych","ranger","glmnet","naniar","tidyverse", "caret", "glmnet", "ggplot2","ggraph","gt","vtable","spatialsample", "stargazer")
 
 ##Cargo la base de datos
 
 db_cln <- readRDS("C:/Users/andye/OneDrive/Documentos/GitHub/Problem_set_2/stores/db_cln.rds")## Cargando la base da datos unificada
+#Ajusto algunos nombres de variables e identifico posibles missing values
 
-###quito de mi dataset las filas correspondientes a variables de texto
-##que se usaron para tener el dato de area,baños y habitaciones.
-
+names(db_cln)
 db_cln <- rename(db_cln, i_rinas = i_riñas)
 db_cln$ESTRATO <- as.factor(db_cln$ESTRATO)
-db_cln <- subset(db_cln, select = -c(bathrooms, area_texto, bano_texto,description,
-                                     rooms,surface_total,surface_covered, title, tipologia_ZIT,
-                                     COD_UPZ, BARRIO, city, property_id, COD_SEC, COD_MZN,
-                                     tegb, teat, UPZ))
-
-###Valido en cuales variables me quedan missing values 
 
 missing_values <- colSums(is.na(db_cln)) #sumo los NA's para cada variable
 missing_table <- data.frame(Variable = names(missing_values), Missing_Values = missing_values) # lo reflejo en un data.frame
@@ -54,7 +46,6 @@ data_imp_1 <- mice::complete(mice_data) # Una recomendación sería imputar sobr
 vars_to_keep <- setdiff(names(db_cln), names(data_imp_1))
 data_to_keep <- db_cln[vars_to_keep]
 data_na <- cbind(data_to_keep, data_imp_1)
-rm(vars_to_keep,data_imp_1,data_to_keep)
 
 data1 <- data_na
 ##########################################################
@@ -63,56 +54,61 @@ data1 <- data_na
 
 ###Para empezar defino set de train y test para toda la base de datos
 
+
+
+# selecciono un primer conjunto de variables de interés (numéricas o factor)####
+data1 <- select(data_na, c(1, 3, 5, 9, 15, 17:29, 31, 33, 46:49, 54:68))
+names(data1)
+
 train_data <- data1 %>%
     filter(sample == "train") %>%
+    select(c(1:4, 6:39)) %>%
+    na.omit() %>%
     st_drop_geometry()
 
 test_data<-data1  %>%
-  filter(sample=="test", LOCALIDAD == "CHAPINERO") %>% 
+  filter(sample=="test") %>% 
+  select(c(1:4, 6:39)) %>%
   st_drop_geometry()
+
+train <- train_data %>% 
+  select(c(2:38)) # omito "property_id" de las predicciones
+
+test <- test_data %>% 
+  select(c(2:38)) # omito "property_id" de las predicciones
+
+#Validamos ubicacion
+
+columnas <- colnames(train)
+for (i in seq_along(columnas)) {
+        ubicacion <- i
+       titulo <- columnas[i]
+       mensaje <- paste("Columna", ubicacion, "-", titulo)
+        print(mensaje)
+}
+
+
+
+
+
+x
 
 #####Regresiones regularizadas
 
 #1.2 Ridge
 
 # Matrix of predictores (de todo el set de datos, voy a incluirlos todos)
-X <- as.matrix(train_data %>% select(bedrooms, property_type, operation_type, estudio,	parqueadero,	balcon,	chimenea,	ascensor,	bbq,	gimnasio,	vigilancia,	jardin,	
-                                     parrilla,	cuarto.servicio,	conjunto.cerrado,	zona.servicio,	area,	banos,	V_REF_22,	ESTRATO,	mzn_densidad,	mzn_n_viv,	
-                                     mzn_n_hab,	dist_CC	, dist_TM,	dist_col,	dist_parq,	geometry,	EPE,	i_rinas,	i_narcoticos,	i_orden,	i_maltrato,	
-                                     d_homicidios,	d_lesiones,	d_hurto_personas,	d_hurto_residencias,	d_hurto_comercio,	d_hurto_autos,	d_hurto_motos,	d_hurto_bici,	
-                                     d_hurto_cel,	d_sexual, d_violencia))
-
-Y <- train_data$price # creo variable predicha
+y <- train_data$price # creo variable predicha
+x <- as.matrix(train_data)
 
 #hacemos prediccion del modelo
+
 ridge1 <- glmnet(
-  x = X,
-  y = Y,
+  x = x,
+  y = y,
   alpha = 0 #ridge
 )
 
 
    
 
-
-
-
-
-
-
-
-
-
-lasso_reg <- linear_reg(penalty = 0.001, mixture = 1) |> 
-  set_engine("glmnet") |> 
-  fit(price ~ year + month + bedrooms + property_type + lat + lon + tasa_hom + tasa_hurtor + n_schools + n_parks + n_ips + tiene_terraza + tiene_parqueadero + tiene_patio + tiene_deposito,
-      data = train_final)
-
-lasso_reg_predict <- predict(lasso_reg_fit, new_data = test_final)
-
-lasso_reg_output <- test_final |> 
-  select(property_id) |> 
-  bind_cols(lasso_reg_predict) |> 
-  rename(price = .pred)
-
-write_csv(lasso_reg_output, "stores/Predictions/lasso_reg.csv")
